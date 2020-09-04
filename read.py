49ffb00ab0a6b384bdecbf9d4074d6e87b603b46
eki66 <soshieki@gmail.com>
2020-09-05 00:34:25 +0800

    add all comments

--- read.py
+++ read.py
@@ -1,15 +1,15 @@
-data = []
-count = 0
-with open ('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 10000 == 0:
-			print(len(data))
+data = [] #建立一個名為data的空清單
+count = 0 #建立一個count變數並放入0, 用來計數
+with open ('reviews.txt', 'r') as f:  # 讀取名為reviews.txt的留言文字檔當作f
+	for line in f: # 將f中每次讀取一筆留言命名為line 的 for 迴圈
+		data.append(line) # 把讀取後的line append加入到data的空清單內
+		count += 1 # count = count + 1 的累加的快寫法
+		if count % 10000 == 0: # 如果count計數和10000的餘數是0時, 意即count計數為10000的倍數時(%是用來求餘數)
+			print(len(data)) # 印出data清單的長度(即總留言筆數)
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 
-sun_len = 0
-for d in data:
-	sun_len = sun_len + len(d)
-print('留言平均長度是', sun_len/len(data))
+sum_len = 0 # 建立一個初始為0, 名為sum_len 加總所有留言的長度的變數
+for d in data: # 將data清單中的每一筆資料命名為d 的 for 迴圈
+	sum_len = sum_len + len(d) # 每一筆留言的長度len(d), 快寫為 sum_len += len(d), 累加所有留言的長度
+print('留言平均長度是', sum_len / len(data)) # 印出留言平均長度 = 總留言長度 / 總留言筆數
